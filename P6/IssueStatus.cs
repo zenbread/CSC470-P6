f163cb9e873edded069def829e0d5427dad016e2
nick <nmcamp87@gmail.com>
2021-11-16 20:47:44 -0500

    Updated to string override

--- P6/IssueStatus.cs
+++ P6/IssueStatus.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; }
         public string Value { get; set; }
 
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }
