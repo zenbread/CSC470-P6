84cc7e595fa8df011918711070ad77ee2f5bad2a
nick <nmcamp87@gmail.com>
2021-11-16 23:12:31 -0500

    asd

---
+++ P6/FormIssueModify.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace P6
+{
+    public partial class FormIssueModify : Form
+    {
+        AppUser _CurrentAppUser;
+        FakeIssueStatusRepository issueStatusRepo = new FakeIssueStatusRepository();
+
+        public FormIssueModify(AppUser appuser)
+        {
+            InitializeComponent();
+            _CurrentAppUser = appuser;
+        }
+
+        private void FormIssueModify_Load(object sender, EventArgs e)
+        {
+            CenterToParent();
+            FakePreferenceRepository fakePreference = new FakePreferenceRepository();
+            string preferredProjectId = fakePreference.GetPreference(_CurrentAppUser.UserName,
+                                                               FakePreferenceRepository.PREFERENCE_PROJECT_ID);
+
+            Int32.TryParse(preferredProjectId, out int ProjectId);
+            FakeIssueRepository fakeIssueRepo = new FakeIssueRepository();
+
+            dataGridView1.Columns.Add("I", "ID");
+            dataGridView1.Columns.Add("T", "Title");
+            dataGridView1.Columns.Add("DDate", "DiscoveryDate");
+            dataGridView1.Columns.Add("D", "Discoverer");
+            dataGridView1.Columns.Add("ID", "InitialDescrition");
+            dataGridView1.Columns.Add("C", "Component");
+            dataGridView1.Columns.Add("S", "Status");
+            List<Issue> issues = fakeIssueRepo.GetAll(ProjectId);
+            foreach(Issue i in issues)
+            {
+                dataGridView1.Rows.Add(i.Id, i.Title, i.DiscoveryDate, i.Discoverer, i.InitialDescription, i.Component, issueStatusRepo.GetValueById(i.IssueStatusId));
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.SelectedRows;
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+
+            }
+            SelectedIssue = new Issue
+            { 
+                Id = (int)row.Cells[0].Value;
+                Title = (string)row.Cells[1].Value;
+                DiscoveryDate = (DateTime)row.Cells[2].Value;
+                Discoverer = (string)row.Cells[3].Value;
+                InitialDescription = (string)row.Cells[4].Value;
+                Component = (string)row.Cells[5].Value;
+                IssueStatusId = issueStatusRepo.GetIdByStatus((string)row.Cells[6].Value);
+            }
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
